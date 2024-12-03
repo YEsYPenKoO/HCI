8267bb0c6ad989d3292983a859243b0d396ba98d
Benjamaxo <bpstannard@gmail.com>
2024-12-03 20:34:05 +0000

    Login dev

---
+++ login/login.js
@@ -0,0 +1,6 @@
+function login() {
+
+    var email=document.getElementById("emailtextbox").value;
+    var password=document.getElementById("passwordtextbox").value;
+    document.getElementById("result").innerHTML=email;
+}
